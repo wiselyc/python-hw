532232934de8ec4f6ff117abfaec390d4f758674
Wisely Chan <Wisely.chan@sjsu.edu>
2021-10-14 20:45:30 -0700

    fixed doc for swap.py

--- swap.py
+++ swap.py
@@ -1,5 +1,5 @@
 def swap_last_item(input_list):
-    #takes in a list and  returns a new list that swapped the first and the last element
+    """takes in a list and  returns a new list that swapped the first and the last element"""
     input_list[0], input_list[-1] = input_list[-1], input_list[0]
     # gets the first and last element and makes the first element = the last and the last element = the first
     return input_list
