18a6d2d4d8ff8a73df40efbe0f426520cfb0c280
Wisely Chan <Wisely.chan@sjsu.edu>
2021-10-14 20:42:54 -0700

    added docstring under functions for calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 def calculator(number1,number2,operator):
+    """this function will use the values the user inputs and calculates it depending on what operation they chose"""
     if operator == '+' :
         return number1 + number2
     elif operator == '-' :
@@ -9,7 +10,7 @@ def calculator(number1,number2,operator):
         if number2 == 0:
             return
         else:
-            return number1//number2
+            return float(number1)//float(number2)
     elif operator == '/' :
         return (float(number1)/float(number2))
     elif operator == '**' :
@@ -18,11 +19,13 @@ def calculator(number1,number2,operator):
         return
 
 def parse_input(input_num1,input_num2,input_operator):
+    """this function will take the input from the user and call the calculator function and then print out the answer"""
     number1 = input_num1
     number2 = input_num2
     operator = input_operator
     print("Enter Equation: " + str(number1) + " " + (operator) + " " + str(number2))
     print(calculator(number1,number2,operator))
+    #this will call the calculator function to print out the answer
 
 parse_input(1,1,'+')
 parse_input(10,11,'+')
